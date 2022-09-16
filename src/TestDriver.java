48b62dc98001a7552d62959d1f0464189e348a26
Esther Eke <eke.esthere@gmail.com>
2022-09-16 20:01:53 +0200

    Initial commit

---
+++ src/TestDriver.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class TestDriver {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println("Hello world!");
+    }
+}
